d9c264c4b8a66fc909e2a614c6b7aff680da5f10
parameshwar <parameshwarr@mediawareonline.com>
2021-08-14 21:30:01 +0530

    space

--- Windchill.java
+++ Windchill.java
@@ -10,6 +10,7 @@ public class Windchill {
 		System.out.println("enter the speed of wind");
 		double vel = sc.nextDouble();
 		double v = Math.pow(vel, 0.16);
+
 		double w = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * v;
 		System.out.println("Effective temprature = " + w);
 	}
